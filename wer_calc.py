a55a24090ba403fc6a378b35cea6ff2b3a726c25
Nina <nina.rao@gmail.com>
2025-12-08 17:19:40 -0500

    Add files via upload
    added check for valid file formats

--- wer_calc.py
+++ wer_calc.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import glob
 import os
 import werpy
-import csv
 import pandas as pd
 import re
 
@@ -12,6 +10,12 @@ import re
 # reference and generated files must be srt or txt
 
 # sys.argv = ['wer_calc.py', '[path/to/reference-directory]', '[path/to/generated-directory]', '[path/to/output.csv]']
+sys.argv = [
+    'wer_calc.py',
+    '/Users/nraogra/Desktop/Captioning/Edited_captions/Captions_first_edits/OH_First_complete/SRT',
+    '/Users/nraogra/Desktop/Captioning/Generated_output/whisper_turbo_detectlang/OH_turbo',
+    '/Users/nraogra/Desktop/OH_test.csv'
+    ]
 
 print("This script matches reference and generated files via csv file.")
 print("Reference and generated files must be .srt or .txt")
@@ -90,48 +94,62 @@ def vtt_to_txt(file_path):
     
 def check_srt(dir:str, filename_float:str):
     """
-    Checks whether a transcript file is in TXT or SRT format.
+    Checks whether a transcript file is in TXT, SRT, or VTT format.
     If the file is in TXT format, returns the data unchanged. 
-    If the file is in SRT format, calls srt_to_txt function and returns the converted data.
+    If the file is in SRT or VTT format, calls srt_to_txt or vtt_to_txt function and returns the converted data.
     """
     filename = str(filename_float)
     file_path = os.path.join(dir, filename)
     if filename.endswith('.txt')==True:
+        validFormat = "Yes"
         f=open(file_path, "r", encoding="utf8")
         data = f.read()
     elif filename.endswith('.srt')==True:
+        validFormat = "Yes"
         data = srt_to_txt(file_path)
-    return data
+    elif filename.endswith('.vtt')==True:
+        validFormat = "Yes"
+        data = vtt_to_txt(file_path)
+    else:
+        validFormat = "No"
+        data = filename
+    return data, validFormat
 
 # Open the output file as a Pandas dataframe
 df= pd.read_csv(outputFile, dtype="str")
 for index, row in df.iterrows():
     # Run check_srt function on both reference and generated files
-    reference_data= check_srt(refDir, row["Reference"])
-    generated_data = check_srt(genDir, row["Generated"])
-    refNormal = werpy.normalize(reference_data)
-    genNormal = werpy.normalize(generated_data)
-    # Check if WER cell is empty
-    if not pd.isna(df.loc[index, 'WER']):
-        while True:
-            wer_cell = df.loc[index, 'WER']
-            print("WER cell not empty (%s), do you want to overwrite? (y/n)" % wer_cell)
-            userDecide = input()
-            if userDecide == "n":
-                break
-            elif userDecide == "y":
-                print("Overwriting WER entry")
-    # Calculate WER from reference and generated data
-                wers = werpy.wers(refNormal, genNormal)
-                werString = str(wers)
-                row["WER"]=werString
-                break
+    reference_data, validFormat = check_srt(refDir, row["Reference"])
+    if validFormat == "No":
+        print(f'Row contains a file {reference_data} that is not the right format; skipping row.')
+    generated_data, validFormat = check_srt(genDir, row["Generated"])
+    if validFormat == "No":
+        print(f'Row contains a file {generated_data} that is not the right format; skipping row.')
     else:
-        wers = werpy.wers(refNormal, genNormal)
-        werString = str(wers)
-        row["WER"]=werString
-
-# Write WER to output file
-df.to_csv(outputFile, index=False)
+        refNormal = werpy.normalize(reference_data)
+        genNormal = werpy.normalize(generated_data)
+        # Check if WER cell is empty
+        if not pd.isna(df.loc[index, 'WER']):
+            while True:
+                wer_cell = df.loc[index, 'WER']
+                print("WER cell not empty (%s), do you want to overwrite? (y/n)" % wer_cell)
+                userDecide = input()
+                if userDecide == "n":
+                    break
+                elif userDecide == "y":
+                    print("Overwriting WER entry")
+        # Calculate WER from reference and generated data and write to output file
+                    wers = werpy.wers(refNormal, genNormal)
+                    werString = str(wers)
+                    row["WER"]=werString
+                    print(werString)
+                    df.to_csv(outputFile, index=False)
+                    break
+        else:
+            wers = werpy.wers(refNormal, genNormal)
+            werString = str(wers)
+            row["WER"]=werString
+            print(werString)
+            df.to_csv(outputFile, index=False)
 
 print('Done')
